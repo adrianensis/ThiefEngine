4017441c5caae2867b32f13397e12f61c75bfc31
AdrianECom <adrian.egea.comenge@gmail.com>
2016-08-25 01:39:37 +0200

    Combination -> Position correction (iterations) + Forces to solve penetrations

--- src/physics/Custom/PhysicsEngine.js
+++ src/physics/Custom/PhysicsEngine.js
@@ -66,7 +66,7 @@ PhysicsEngine.prototype.simulate = function (dt){
   }
 
   var it = 0;
-  var maxIt = 20;
+  var maxIt = 10;
   var first = true;
   var status = Collider.STATUS_NONE;
 
@@ -106,7 +106,7 @@ PhysicsEngine.prototype.simulate = function (dt){
           // else
           //   targetTime = -(deltaTime);
 
-          if(targetTime > 0.001)
+          if(targetTime > 0.0001)
             tryAgain = true;
 
           // console.log("penetration");
