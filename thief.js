d565101f169d698cd66b6709f35ebeea939b8d1f
Adrián Egea Comenge <adrian.egea.comenge@gmail.com>
2017-06-01 16:14:25 +0200

    regenerate thief.js

--- thief.js
+++ thief.js
@@ -2242,9 +2242,6 @@ SpriteBatch.prototype.bind = function (){
     gl.vertexAttribPointer(1, 2, gl.FLOAT, false, 0, 0);
 
 
-
-
-
     // NORMALS
     gl.enableVertexAttribArray(3);
     SpriteBatch.vboNormal = gl.createBuffer();
@@ -2261,10 +2258,6 @@ SpriteBatch.prototype.bind = function (){
     // Finised setting up VAO
     vao_ext.bindVertexArrayOES(null);
 
-    // var children = this.getChildren();
-    // for (var i = 0; i < children.length; i++) {
-    // 	children[i].bind();
-    // }
 
     }
 
@@ -2272,13 +2265,6 @@ SpriteBatch.prototype.bind = function (){
   // COLOR
   gl.enableVertexAttribArray(2);
   this.vboColor = gl.createBuffer();
-  // gl.bindBuffer(gl.ARRAY_BUFFER, this.vboColor);
-  // var color = [];
-  // for (var i = 0; i < SpriteBatch.mesh.getVerticesData().length/4; i++) {
-  // 	color = color.concat(this.material.color.getVector().toArray(());
-  // }
-  // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(color), gl.STATIC_DRAW); // TODO DYNAMIC
-  // gl.vertexAttribPointer(2, 4, gl.FLOAT, false, 0, 0);
 
 };
 
@@ -2314,8 +2300,7 @@ SpriteBatch.prototype.render = function (layer){
 
   gl.enableVertexAttribArray(0);
 
-  // if(SpriteBatch.mesh.hasTexture())
-    gl.enableVertexAttribArray(1);
+  gl.enableVertexAttribArray(1);
 
   gl.enableVertexAttribArray(3);
 
@@ -2335,10 +2320,11 @@ SpriteBatch.prototype.render = function (layer){
 
         gl.bindBuffer(gl.ARRAY_BUFFER, this.vboColor);
 
+        // create color buffer
       	var color = [];
-      	for (var j = 0; j < SpriteBatch.mesh.getVerticesData().length/4; j++) {
+      	for (var j = 0; j < SpriteBatch.mesh.getVerticesData().length/4; j++)
       	   color = color.concat(renderer.getMaterial().getColor().toArray());
-      	}
+
       	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(color), gl.STATIC_DRAW);
       	gl.vertexAttribPointer(2, 4, gl.FLOAT, false, 0, 0);
 
